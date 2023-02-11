a69bf71bd9312a44a12e04121c801ce0dda1867c
yana-kushmar <yana.puzevich@gmail.com>
2023-02-11 15:36:44 +0800

    dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import './App.css';
 import Header from "./components/Header/Header";
 import NavigationBar from "./components/NavigationBar/navigationBar";
 import ProfileContent from "./components/ProfileContent/ProfileContent";
+import Dialogs from "./components/Dialogs/Dialogs";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <Header />
-            <NavigationBar />
-            <ProfileContent />
-
+            <Header/>
+            <NavigationBar/>
+            {/*<ProfileContent />*/}
+            <Dialogs/>
         </div>
 
     );
